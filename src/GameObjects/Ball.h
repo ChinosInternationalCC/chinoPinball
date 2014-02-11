22a2e45c89ac7257cde34bd4c6083b56b45931ee
Kuba Markiewicz <hello@jacobmarcus.com>
2014-02-11 21:25:24 +0100

    reset the ball with ENTER

--- src/GameObjects/Ball.h
+++ src/GameObjects/Ball.h
@@ -19,19 +19,31 @@ public:
     void setup(ofxBulletWorldRigid &world, ofVec3f position);
     void update(void);
     void draw(void);
+    
+    ofVec3f startPosition;
+    float radius = .3;
+    float mass = 2;
+    
+    ofxBulletWorldRigid world;
+    
+	void keyPressed(ofKeyEventArgs& key);
+	void keyReleased(ofKeyEventArgs& key);
+    
     bool isInsideScenario(ofBoxPrimitive box);
     bool setGameOverBall(void); //callback from GameOverCollision,
                                 //returns true if the status was changed, false if already set
-    
     enum BallStatus{
         BallStatusWaiting = 0,
         BallStatusPlaying,
         BallStatusGameOver
     };
+    
+    void reset();
 
 private:
     BallStatus m_status; // waiting, playing, game over
    // ofxBulletSphere *BulletBallShape; // generic bullet shape object
+    
 };
 
 #endif /* defined(__ofxBulletEventsExample__Ball__) */
