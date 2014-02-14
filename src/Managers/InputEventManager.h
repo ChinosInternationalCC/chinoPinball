770c0f98a093b1add58860c5bcdc9c4c13b74a0b
Ovidiu Cincheza <ovidiu@Mac-mini-de-Ovidiu.local>
2014-02-14 17:06:26 +0100

    added ExternalsConfig.h

--- src/Managers/InputEventManager.h
+++ src/Managers/InputEventManager.h
@@ -9,15 +9,19 @@
 #ifndef __chinoPinball__InputEventManager__
 #define __chinoPinball__InputEventManager__
 
+
+#include "ExternalsConfig.h"
 #include "ofMain.h"
+#ifdef USE_OSC /* configured in ExternalsConfig.h */
 #include "ofxOsc.h"
+#endif
 
 /* this class is the entry point from all the interfaces suported
  * eg: keyboard, OSC TBD
  * should be extended by the PinballChino manager
  * PinbalChinoManager should implement the pure virtual methods
  */
-class InputEventManager: public ofxOscReceiver
+class InputEventManager
 {
 public:
     InputEventManager();
@@ -27,8 +31,9 @@ public:
     virtual void onMoveBallLauncherEvent(void) = 0;
     virtual void onRestartGameEvent(void) = 0;
     
-
-    
+#ifdef USE_OSC
+    ofxOscReceiver oscReceiver;
+#endif
     
 };
 
