c3d31b46d5a35b9267501b1fed5ef846e845eb37
carles <Admin@dude.local>
2014-03-28 20:39:36 +0100

    Add ArduinoCode for buttons and Rele

---
+++ src/arduComunicationManager.cpp
@@ -0,0 +1,9 @@
+//
+//  arduOutputManager.cpp
+//  chinoPinball
+//
+//  Created by carles on 28/03/14.
+//
+//
+
+#include "arduOutputManager.h"
---
+++ src/arduComunicationManager.h
@@ -0,0 +1,14 @@
+//
+//  arduOutputManager.h
+//  chinoPinball
+//
+//  Created by carles on 28/03/14.
+//
+//
+
+#ifndef __chinoPinball__arduOutputManager__
+#define __chinoPinball__arduOutputManager__
+
+#include <iostream>
+
+#endif /* defined(__chinoPinball__arduOutputManager__) */
